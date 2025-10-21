037441a8fbe3480812fc841a36b8c2a58a0e536a
Ozan Oner <ozan.oner@gmail.com>
2025-10-21 16:43:54 +0100

    9 implement the blinky button example (#10)
    * test button
    
    * timer tested
    
    * feedback

--- examples/blinky_button/main/bsp_esp32.c
+++ examples/blinky_button/main/bsp_esp32.c
@@ -1,9 +1,67 @@
-
 #include "bsp.h"
+#include "driver/gpio.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 
-void BSP_init(void) { }
-void BSP_start(void) { }
-void BSP_led0_on(void) { }
-void BSP_led0_off(void) { }
-void BSP_led1_on(void) { }
-void BSP_led1_off(void) { }
+#define LED_RED GPIO_NUM_2
+#define LED_BLUE GPIO_NUM_4
+#define BTN_RED GPIO_NUM_13
+#define DELAY_TIME 200
+
+static void button_isr_handler(void* arg)
+{
+    BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+
+    if (gpio_get_level(BTN_RED) == 0)
+    {
+        static Event const buttonPressedEvt = {BUTTON_PRESSED_SIG};
+        Active_postFromISR(AO_blinkyButton, &buttonPressedEvt, &xHigherPriorityTaskWoken);
+    }
+    else
+    {
+        static Event const buttonReleasedEvt = {BUTTON_RELEASED_SIG};
+        Active_postFromISR(AO_blinkyButton, &buttonReleasedEvt, &xHigherPriorityTaskWoken);
+    }
+}
+
+void BSP_init(void)
+{
+    // Configure button
+    gpio_install_isr_service(0);
+    gpio_reset_pin(BTN_RED);
+    gpio_set_direction(BTN_RED, GPIO_MODE_INPUT);
+    gpio_pullup_en(BTN_RED);
+    gpio_set_intr_type(BTN_RED, GPIO_INTR_ANYEDGE);
+    gpio_isr_handler_add(BTN_RED, button_isr_handler, NULL);
+
+    // Configure LED
+    gpio_reset_pin(LED_RED);
+    gpio_set_direction(LED_RED, GPIO_MODE_OUTPUT);
+    gpio_reset_pin(LED_BLUE);
+    gpio_set_direction(LED_BLUE, GPIO_MODE_OUTPUT);
+}
+
+void BSP_start(void)
+{
+    /* nothing to do */
+}
+
+void BSP_led0_on(void)
+{
+    gpio_set_level(LED_RED, 1);
+}
+
+void BSP_led0_off(void)
+{
+    gpio_set_level(LED_RED, 0);
+}
+
+void BSP_led1_on(void)
+{
+    gpio_set_level(LED_BLUE, 1);
+}
+
+void BSP_led1_off(void)
+{
+    gpio_set_level(LED_BLUE, 0);
+}
--- examples/blinky_button/main/main.c
+++ examples/blinky_button/main/main.c
@@ -1,65 +1,96 @@
-/* Interrupt Example
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
-
-#include <stdio.h>
+/*
+ * Lab Project: BlinkyButton/Button with RTOS (FreeRTOS) and blocking
+ * Board: ESP32*
+ *
+ * Copyright (C) 2005-2020 Quantum Leaps, LLC. All rights reserved.
+ *
+ * Contact information:
+ * <www.state-machine.com>
+ * <info@state-machine.com>
+ */
 
 #include "FreeAct.h"
-#include "driver/gpio.h"
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
+#include "bsp.h"
+#include "esp_log.h"
 
-#define LED_RED GPIO_NUM_2
-#define BTN_RED GPIO_NUM_13
-#define DELAY_TIME 200
+#define TAG "app"
 
-volatile bool button_pressed = false;
-Active*       AO_blinkyButton;
-
-static void gpio_isr_handler(void* arg)
+/* The BlinkyButton AO */
+typedef struct
 {
-    button_pressed = true;
+    Active super; /* inherit Active base class */
+    /* add private data for the AO... */
+    TimeEvent te;
+    bool      isLedOn;
+} BlinkyButton;
+
+void BlinkyButton_ctor(BlinkyButton* const me);
+
+static void BlinkyButton_dispatch(BlinkyButton* const me, Event const* const e)
+{
+    switch (e->sig)
+    {
+        case INIT_SIG: /* intentionally fall through... */
+        case TIMEOUT_SIG:
+        {
+            if (!me->isLedOn)
+            { /* LED not on */
+                BSP_led1_on();
+                me->isLedOn = true;
+                TimeEvent_arm(&me->te, 200);
+            }
+            else
+            { /* LED is on */
+                BSP_led1_off();
+                me->isLedOn = false;
+                TimeEvent_arm(&me->te, 800);
+            }
+            break;
+        }
+        case BUTTON_PRESSED_SIG:
+        {
+            BSP_led0_on();
+            break;
+        }
+        case BUTTON_RELEASED_SIG:
+        {
+            BSP_led0_off();
+            break;
+        }
+        default:
+        {
+            break;
+        }
+    }
 }
 
-void button_config()
+void BlinkyButton_ctor(BlinkyButton* const me)
 {
-    gpio_install_isr_service(0);
-    printf("configuring button\n");
-    gpio_reset_pin(BTN_RED);
-    gpio_set_direction(BTN_RED, GPIO_MODE_INPUT);
-    gpio_pullup_en(BTN_RED);
-    gpio_set_intr_type(BTN_RED, GPIO_INTR_POSEDGE);
-    gpio_isr_handler_add(BTN_RED, gpio_isr_handler, NULL);
-    printf("config complete\n");
+    Active_ctor(&me->super, (DispatchHandler)&BlinkyButton_dispatch);
+    me->te.type = TYPE_ONE_SHOT;
+    TimeEvent_ctor(&me->te, TIMEOUT_SIG, &me->super);
+    me->isLedOn = false;
 }
 
-void led_config()
-{
-    gpio_reset_pin(LED_RED);
-    gpio_set_direction(LED_RED, GPIO_MODE_OUTPUT);
-}
+static StackType_t  blinkyButton_stack[configMINIMAL_STACK_SIZE * 2]; /* task stack */
+static Event*       blinkyButton_queue[10];
+static BlinkyButton blinkyButton;
+Active*             AO_blinkyButton = &blinkyButton.super;
 
 void app_main()
 {
-    uint8_t led_value = 0;
+    ESP_LOGI(TAG, "BlinkyButton example start");
+    BSP_init(); /* initialize the BSP */
 
-    button_config();
-    led_config();
+    /* create and start the BlinkyButton AO */
+    BlinkyButton_ctor(&blinkyButton);
+    Active_start(AO_blinkyButton,                                             ///< Active object to start
+                 1U,                                                          ///< Task priority (1-based)
+                 blinkyButton_queue,                                          ///< event queue storage
+                 sizeof(blinkyButton_queue) / sizeof(blinkyButton_queue[0]),  ///< queue length
+                 blinkyButton_stack,                                          ///< stack storage
+                 sizeof(blinkyButton_stack),                                  ///< stack size in bytes
+                 0U);
 
-    while (1)
-    {
-        if (button_pressed)
-        {
-            printf("*\n");
-            button_pressed = false;
-            led_value      = !led_value;
-            gpio_set_level(LED_RED, led_value);
-        }
-        vTaskDelay(DELAY_TIME / portTICK_PERIOD_MS);
-    }
+    BSP_start(); /* configure and start interrupts */
 }
